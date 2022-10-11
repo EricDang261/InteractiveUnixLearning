5594e54c8b677201f63928f491f41c91f412aaa5
Thinh P. Dang <tpd261@uw.edu>
2022-10-11 00:38:56 -0700

    registraton form

--- src/routes/SignUpForm.jsx
+++ src/routes/SignUpForm.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import './SignUpForm.css'
+import {Link} from 'react-router-dom'
 
 const SignUpForm = () => {
+
   return (
-    <div>SignUpForm</div>
+    <div className= "SignUp-form-container" >
+    <form>
+          <div className="form-inner">
+              <h2>User Register</h2>
+
+              <div className="form-group">
+                  <label htmlFor="name"> Name: </label>
+                  <input type="text" name="name" id="name"/>
+              </div>  {/* end of the form-group div */}    
+
+              <div className="form-group">
+                  <label htmlFor="email">Email: </label>
+                  <input type="email" name="email" id="email"/>
+              </div> {/* end of the form-group div */}
+
+              <div className="form-group">
+                  <label htmlFor="password">Password: </label>
+                  <input type="password" name="password" id="password"  />
+              </div> {/* end of the form-group div */}
+
+              <div className="form-group">
+                  <label htmlFor="password">Re-type Password: </label>
+                  <input type="password" name="password" id="password"  />
+              </div> {/* end of the form-group div */}
+
+              <input type="submit" value="REGISTER" />  
+              <div className='link-btn'>
+                 Already have an account? <Link to="/login">Log In</Link>
+            </div>
+          </div> {/* end of the form innter div */}
+    </form>
+  </div> /* end of the form-group div */ 
   )
 }
-
 export default SignUpForm

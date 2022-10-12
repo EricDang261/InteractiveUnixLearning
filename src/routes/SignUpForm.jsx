7ef31bbf10c88cec445e7f45d35aebf7a5a8eb51
Ash-TB <sunrise4ever37@yahoo.com>
2022-10-12 01:39:57 -0700

    completed scrolling feature on webpages

--- src/routes/SignUpForm.jsx
+++ src/routes/SignUpForm.jsx
@@ -1,6 +1,5 @@
 
-import React, { useState } from 'react'
-import {useNavigate} from 'react-router-dom'
+import React from 'react'
 import './SignUpForm.css'
 import {Link} from 'react-router-dom'
 
